23fed1ccfd4e7782f5e1262153d103a4485a4bb9
fumikun <fumikun6130@gmail.com>
2023-06-05 23:52:14 +0900

    add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        about: "url('/src/assets/img/profile_background.png')",
+      },
+    },
   },
   plugins: [],
 };
