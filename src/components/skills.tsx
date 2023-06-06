db16f6a12fc5e5380360ae4ae61fcf6b8d97b59e
fumikun <fumikun6130@gmail.com>
2023-06-06 20:23:10 +0900

    Simple iconsに変更

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,50 +1,51 @@
-import { TbBrandGolang, TbBrandPython, TbBrandJavascript, TbBrandTypescript, TbBrandHtml5, TbBrandCss3, TbBrandDocker } from "react-icons/tb"
+// import { SiGolang, SiPython, SiJavascript, SiTypescript, SiHtml5, SiCss3, SiDocker } from "react-icons/tb"
+import { SiGo, SiPython, SiJavascript, SiTypescript, SiHtml5, SiCss3, SiDocker } from "react-icons/si";
 export const Skills = () => {
     return (
         <>
             <div className="bg-slate-900">
-                <div className="font-mono text-3xl font-bold text-center text-lime-500">Skills</div>
+                <div className="font-mono text-3xl font-bold text-center text-lime-400">Skills</div>
                 <div className="bg-slate-900">
-                    <div className="grid grid-cols-7">
+                    <div className="grid md:grid-cols-7 sm:grid-cols-2">
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandGolang size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiGo size={40}  />
                                 <div className="text-center">Go</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandPython size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiPython size={40} />
                                 <div className="text-center">Python</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandJavascript size={50} />
+                            <div className="flex items-center justify-start p-1 ">
+                                <SiJavascript size={40} />
                                 <div className="text-center">JavaScript</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandTypescript size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiTypescript size={40} />
                                 <div className="text-center">TypeScript</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandHtml5 size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiHtml5 size={40} />
                                 <div className="text-center">HTML</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandCss3 size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiCss3 size={40} />
                                 <div className="text-center">CSS</div>
                             </div>
                         </div>
                         <div className="grid m-3 text-center border rounded justify-items-center border-slate-100 text-slate-100">
-                            <div className="flex items-center justify-start">
-                                <TbBrandDocker size={50} />
+                            <div className="flex items-center justify-start p-1">
+                                <SiDocker size={40} />
                                 <div className="text-center">Docker</div>
                             </div>
                         </div>
