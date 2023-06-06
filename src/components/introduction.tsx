e9451ea4b1f97fc4018b807b1048911e679e2400
fumikun <fumikun6130@gmail.com>
2023-06-06 14:50:45 +0900

    change color

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -1,49 +1,34 @@
 import icon from "../assets/img/computer_switching_hub.png"
-import { FaGithub, FaMapMarkerAlt } from "react-icons/fa";
-import { TbBrandGolang, TbBrandTypescript, TbBrandJavascript, TbBrandPython, TbBrandHtml5, TbBrandCss3, TbBrandPhp } from "react-icons/tb"
+import { FaGithub } from "react-icons/fa";
 export const Introduction = () => {
     return (
         <>
-            <div className="items-center justify-center p-5 font-mono text-center bg-center bg-auto bg-cover bg-about text-slate-200" id="#about">
-                <div className="font-mono text-3xl font-bold text-center text-lime-500">About me</div>
-                <div className="items-center justify-center md:flex">
-                    <div className="items-center justify-center">
-                        <img src={icon} className="w-1/2 pt-1 pb-1 m-1 mx-auto bg-white rounded-full" />
-                    </div>
-                    <div className="items-center justify-center h-full p-3 m-1 text-black bg-white rounded md:w-full lg:w-fit">
-                        <div className="font-mono text-xl font-bold text-lime-500">fumikun's Property</div>
-                        某国立高専に通う高専生です。<br></br>
-                        Web系を中心としたネットワークに興味があります。<br></br>
-                        フロントエンドとバックエンドを一人で細々作ってます。
-                    </div>
-
-                </div>
-                <br></br>
-                <div className="flex items-center justify-center">
-                    <div className="items-center justify-center h-full p-3 m-1 text-black bg-white rounded md:w-full lg:w-fit">
-                        <div className="items-center text-black bg-white rounded ">
-                            <p className="font-mono text-xl font-bold text-lime-500">Language</p>
-                            <div className="flex justify-center">
-                                <TbBrandGolang size={30} />
-                                <TbBrandTypescript size={30} />
-                                <TbBrandJavascript size={30} />
-                                <TbBrandPython size={30} />
-                                <TbBrandHtml5 size={30} />
-                                <TbBrandCss3 size={30} />
-                                <TbBrandPhp size={30} />
-                            </div>
+            <div className="bg-auto bg-cover bg-about text-slate-100 bg-slate-900">
+                <div className="items-center justify-center p-5 font-mono text-center bg-center text-slate-200" id="#about">
+                    <div className="font-mono text-3xl font-bold text-center text-lime-500">About me</div>
+                    <div className="items-center justify-center p-3 m-1 md:flex">
+                        <div className="items-center justify-center">
+                            <img src={icon} className="w-1/2 pt-1 pb-1 m-1 mx-auto bg-white rounded-full" />
                         </div>
-                    </div>
-                    <div className="items-center justify-center h-full p-3 m-1 text-black bg-white rounded md:w-full lg:w-fit">
-                        <div className="items-center text-black bg-white rounded ">
-                            <p className="font-mono text-xl font-bold text-lime-500">Link</p>
-                            <div className="flex justify-center">
-                                <div className="flex items-center">
-                                    <a className="flex items-center" href="https://github.com/fumikun">
-                                        <FaGithub size={30} />
-                                        <p className="pt-1 ps-1">GitHub</p>
+                        <div className="items-center justify-center h-full p-3 m-1 mt-3 border border-white rounded bg-slate-900 md:w-full lg:w-fit">
+                            <div className="font-mono text-xl font-bold text-lime-500">fumikun's Property</div>
+                            某国立高専に通う高専生です。<br></br>
+                            Web系を中心としたネットワークに興味があります。<br></br>
+                            フロントエンドとバックエンドを一人で細々作ってます。
+                        </div>
 
-                                    </a>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div className="items-center justify-center h-full p-3 m-1 border rounded text-slate-100 bg-slate-900 md:w-full lg:w-fit">
+                            <div className="items-center rounded bg-slate-900 text-slate-100 ">
+                                <p className="font-mono text-xl font-bold text-lime-500">Link</p>
+                                <div className="flex justify-center">
+                                    <div className="flex items-center">
+                                        <a className="flex items-center" href="https://github.com/fumikun">
+                                            <FaGithub size={30} />
+                                            <p className="pt-1 ps-1">GitHub</p>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
