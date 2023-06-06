e6bfede9b1eee2da6e52dc6560fbf627d83cf32a
fumikun <fumikun6130@gmail.com>
2023-06-06 14:50:58 +0900

    add works

---
+++ src/components/works.tsx
@@ -0,0 +1,10 @@
+export const Works = () => {
+    return (
+        <>
+            <div className="font-mono text-3xl font-bold text-center text-lime-500">Works</div>
+            <div className="flex">
+                <div className="">Higumabot</div>
+            </div>
+        </>
+    )
+}
